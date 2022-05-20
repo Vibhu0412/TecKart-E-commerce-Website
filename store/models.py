89a9fb12ca179bf94f65fdb1b5c17ab00dd4591a
Vaibhav Purohit <vbpurohit1948@gmail.com>
2022-05-20 15:57:44 +0530

    TecKart E-commerce Website

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from category.models import Category
 
+
 # Custom Push 123456
 
 # Create your models here.
@@ -22,4 +23,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
-
