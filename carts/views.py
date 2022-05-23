0008be312a52688c4b6bfa5457bf5b1a521d073f
Vaibhav Purohit <vbpurohit1948@gmail.com>
2022-05-23 18:22:37 +0530

    TecKart E-commerce Website

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -57,6 +58,9 @@ def remove_cart_item(request, product_id):
 
 
 def cart(request, total=0, quantity=0, cart_items=None):
+    tax = 0
+    grand_total = 0
+
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
@@ -68,7 +72,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         tax = int((2 * total)/100)
         grand_total = total + tax
 
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass # just ignore
 
     context = {
--- store/views.py
+++ store/views.py
@@ -4,19 +4,33 @@ from category.models import Category
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
 
+from django.http import HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
+
 
 # Create your views here.
 def store(request, category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+
+        paginator = Paginator(products, 3)  # Setting up 5 products to show each page.
+        page = request.GET.get('page')  # fetching the page value viz https://abc.com/?page=2
+        paged_products = paginator.get_page(page)  # Getting the desired product per specific page.
+
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+
+        paginator = Paginator(products, 3)   # Setting up 5 products to show each page.
+        page = request.GET.get('page')      # fetching the page value viz https://abc.com/?page=2
+        paged_products = paginator.get_page(page)   # Getting the desired product per specific page.
+
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
@@ -36,3 +50,22 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) |
+                                                                        Q(product_name__icontains=keyword))
+            # Using 'Q' for complex queries.....filter(abc=keyword, xyz=keyword) will perform AND operation not OR
+            product_count = products.count()
+        else:
+            products = Product.objects.all().filter(is_available=True).order_by('id')
+            product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
